6a06e2a074e56649477f40a4e12b3eb2971e2c82
blubrackets <6eckermans2015@gmail.com>
2016-05-18 13:46:01 -0400

    Finished product

--- cell.js
+++ cell.js
@@ -1,7 +1,27 @@
-function Cell() {
-	this.pos = createVector(random(width), random(height));
-	this.r = 80;
-	this.co = color(0, random(100, 255), random(50, 100));
+function Cell(pos, r, co) {
+	if (pos) {
+		this.pos = pos.copy();
+	} else {
+		this.pos = pos || createVector(random(width), random(height));
+	}
+	this.r = r || 80;
+	this.co = co || color(0, random(100, 255), random(50, 100), 100);
+
+	this.clicked = function(x, y) {
+		var d = dist(this.pos.x, this.pos.y, x, y);
+
+		if (d < this.r) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	this.mitosis = function() {
+		this.pos.vel += random(10);
+		var cell = new Cell(this.pos, this.r * 0.8, this.co);
+		return cell;
+	}
 
 	this.move = function() {
 		var vel = p5.Vector.random2D();
--- frame.js
+++ frame.js
@@ -1,12 +1,25 @@
-var c;
+var cells = [];
 
 function setup() {
-	createCanvas(400, 400);
-	c = new Cell;
+	createCanvas(700, 700);
+	cells.push(new Cell());
+	cells.push(new Cell());
 }
 
 function draw() {
-	background(51);
-	c.move();
-	c.show();
+	background(200);
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].move();
+		cells[i].show();
+	}
+}
+
+function mousePressed() {
+	for (var i = cells.length-1; i >= 0; i--) {
+		if (cells[i].clicked(mouseX, mouseY)) {
+			cells.push(cells[i].mitosis());
+			cells.push(cells[i].mitosis());
+			cells.splice(i, 1);
+		}
+	}
 }
