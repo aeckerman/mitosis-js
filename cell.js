8671a2f55d760ce5076f87dbd184b39c85f1b135
Andrew Eckerman <6eckermans2015@gmail.com>
2016-05-19 21:31:38 -0500

    nice design

--- cell.js
+++ cell.js
@@ -1,11 +1,12 @@
-function Cell(pos, r, co) {
+function Cell(pos, r, co, ou) {
 	if (pos) {
 		this.pos = pos.copy();
 	} else {
 		this.pos = pos || createVector(random(width), random(height));
 	}
 	this.r = r || 80;
-	this.co = co || color(0, random(100, 255), random(50, 100), 100);
+	this.co = co || color(0, 155, 88, 40);
+	this.ou = ou || color(0, 155-50, 88-40, 40-20);
 
 	this.clicked = function(x, y) {
 		var d = dist(this.pos.x, this.pos.y, x, y);
@@ -29,7 +30,8 @@ function Cell(pos, r, co) {
 	}
 
 	this.show = function() {
-		noStroke();
+		stroke(this.ou);
+		strokeWeight(5);
 		fill(this.co);
 		ellipse(this.pos.x, this.pos.y, this.r, this.r);
 	}
